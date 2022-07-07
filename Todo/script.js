2c09debd9c835a3a0fdfec5e262325319c913863
Ritukalpa Gogoi <ritukalpa.gogoi99@gmail.com>
2022-07-08 00:45:42 +0530

    delete function added

--- Todo/script.js
+++ Todo/script.js
@@ -2,7 +2,7 @@ const input = document.querySelector(".input-field");
 
 const btn = document.querySelector("button");
 
-const list = document.querySelector(".task-list");
+const list = document.querySelector(".list");
 
 btn.addEventListener("click", (e) => {
   // No Refrest
@@ -11,10 +11,14 @@ btn.addEventListener("click", (e) => {
   // Creating and Assinging task
   const task = document.createElement("div");
   task.innerText = input.value;
-
+  task.classList.add("list-item");
   // Appending to DOM
   list.appendChild(task);
 
   //Clearing value from input
   input.value = "";
+
+  task.addEventListener("click", () => {
+    task.remove();
+  });
 });
